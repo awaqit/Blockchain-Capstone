15dbad79c52a94a3286ecb3094745e744407a182
Abdullah Waqet <abdullah.waqet@hotmail.com>
2019-08-23 22:28:22 +0300

    test SolnSquareVerifier

--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,3 +1,46 @@
-// Test if a new solution can be added for contract - SolnSquareVerifier
+const SolnSquareVerifier = artifacts.require('SolnSquareVerifier');
+const Verifier = artifacts.require('Verifier');
 
-// Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+const proofs = require('../../zokrates/code/square/proof');
+const index = 1;
+contract('SolnSquareVerifier', accounts => {
+
+    describe('Test SolnSquareVerifier', () => {
+
+        beforeEach(async () => {
+                const verifier = await Verifier.new({
+                    from: accounts[0]
+                });
+                this.contract = await SolnSquareVerifier.new(verifier.address, {
+                    from: accounts[0]
+                });
+            });
+
+        // Test if a new solution can be added for contract - SolnSquareVerifier
+        it("should be able to add new solution for contract", async ()=>{
+            let proof = proofs.proof;
+            let inputs = proofs.inputs;
+            const result = await this.contract.mintNFT.call(
+                accounts[1],
+                index,
+                proof.a,
+                proof.b,
+                proof.c,
+                inputs
+            );
+
+            assert.equal(result, true, "solution can't be added duplicate");
+        });
+
+        // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+        it("should ERC721 token minted for contract", async ()=>{
+            const result = await this.contract.mint.call(accounts[1], index, "", {
+                from: accounts[0]
+            });
+
+            assert.equal(result, true, 'ERC721 token was not minted');
+        });
+    });
+
+
+});
